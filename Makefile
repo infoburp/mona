219457e6f2a78a78f01435369f3ce5401e8f6e29
Kyle McLamb <kjmclamb@gmail.com>
2014-05-07 21:11:43 -0400

    Preliminary getopt parsing.
    This moves the code more firmly into posix-land, but there are a bunch
    of other posix calls still in the code anyways. Sorry, windows users.

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 mona: Makefile mona.c
-	gcc -DSHOWWINDOW -Wall -std=gnu99 -pedantic -O3 `sdl2-config --libs --cflags` `pkg-config --libs --cflags cairo cairo-xlib` mona.c -o mona
+	gcc -DWITH_SDL -Wall -std=gnu99 -pedantic -O3 `sdl2-config --libs --cflags` `pkg-config --libs --cflags cairo cairo-xlib` mona.c -o mona
 clean:
 	rm -f mona
 
--- mona.c
+++ mona.c
@@ -17,6 +17,7 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <limits.h>
+#include <stdbool.h>
 
 #include <cairo.h>
 #include <cairo-xlib.h>
@@ -24,40 +25,43 @@
 #define RANDINT(max) (int)((random() / (double)RAND_MAX) * (max))
 #define RANDDOUBLE(max) ((random() / (double)RAND_MAX) * max)
 #define ABS(val) ((val) < 0 ? -(val) : (val))
-#define CLAMP(val, min, max) ((val) < (min) ? (min) : \
-                              (val) > (max) ? (max) : (val))
+#define CLAMP(val, min, max) ((val) < (min) ? (min) : (val) > (max) ? (max) : (val))
 
 int WIDTH;
 int HEIGHT;
 
+bool SHOW_WINDOW = true;
+bool timelimit = false;
+bool dump = false;
+
 //////////////////////// X11 stuff ////////////////////////
-#ifdef SHOWWINDOW
+#ifdef WITH_SDL
 
 #include "SDL.h"
 
-SDL_Window * win;
-SDL_Surface * screen;
+SDL_Window *win;
+SDL_Surface *screen;
 
 void x_init(void)
 {
-    if(SDL_Init(SDL_INIT_VIDEO) != 0)
-    {
-        fprintf(stderr, "ERR: %s\n", SDL_GetError());
-        exit(1);
-    }
-	win = SDL_CreateWindow("mona",
-			SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-			WIDTH, HEIGHT,
-			0);
+	if (SHOW_WINDOW) {
+		if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+			fprintf(stderr, "ERR: %s\n", SDL_GetError());
+			exit(1);
+		}
+		win = SDL_CreateWindow("mona",
+				SDL_WINDOWPOS_UNDEFINED,
+				SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, 0);
 
-	if (win == NULL)
-	{
-        fprintf(stderr, "ERR: %s\n", SDL_GetError());
-        exit(1);
+		if (win == NULL) {
+			fprintf(stderr, "ERR: %s\n", SDL_GetError());
+			exit(1);
+		}
+
+		screen = SDL_GetWindowSurface(win);
 	}
-
-	screen = SDL_GetWindowSurface(win);
 }
+
 #else
 void x_init(void)
 {
@@ -66,12 +70,12 @@ void x_init(void)
 //////////////////////// end X11 stuff ////////////////////////
 
 typedef struct {
-    double x, y;
+	double x, y;
 } point_t;
 
 typedef struct {
-    double r, g, b, a;
-    point_t points[NUM_POINTS];
+	double r, g, b, a;
+	point_t points[NUM_POINTS];
 } shape_t;
 
 shape_t dna_best[NUM_SHAPES];
@@ -81,301 +85,318 @@ int mutated_shape;
 
 void draw_shape(shape_t * dna, cairo_t * cr, int i)
 {
-    cairo_set_line_width(cr, 0);
-    shape_t * shape = &dna[i];
-    cairo_set_source_rgba(cr, shape->r, shape->g, shape->b, shape->a);
-    cairo_move_to(cr, shape->points[0].x, shape->points[0].y);
-    for(int j = 1; j < NUM_POINTS; j++)
-        cairo_line_to(cr, shape->points[j].x, shape->points[j].y);
-    cairo_fill(cr);
+	cairo_set_line_width(cr, 0);
+	shape_t *shape = &dna[i];
+	cairo_set_source_rgba(cr, shape->r, shape->g, shape->b, shape->a);
+	cairo_move_to(cr, shape->points[0].x, shape->points[0].y);
+	for (int j = 1; j < NUM_POINTS; j++)
+		cairo_line_to(cr, shape->points[j].x, shape->points[j].y);
+	cairo_fill(cr);
 }
 
 void draw_dna(shape_t * dna, cairo_t * cr)
 {
-    cairo_set_source_rgb(cr, 1, 1, 1);
-    cairo_rectangle(cr, 0, 0, WIDTH, HEIGHT);
-    cairo_fill(cr);
-    for(int i = 0; i < NUM_SHAPES; i++)
-        draw_shape(dna, cr, i);
+	cairo_set_source_rgb(cr, 1, 1, 1);
+	cairo_rectangle(cr, 0, 0, WIDTH, HEIGHT);
+	cairo_fill(cr);
+	for (int i = 0; i < NUM_SHAPES; i++)
+		draw_shape(dna, cr, i);
 }
 
 void init_dna(shape_t * dna)
 {
-    for(int i = 0; i < NUM_SHAPES; i++)
-    {
-        for(int j = 0; j < NUM_POINTS; j++)
-        {
-            dna[i].points[j].x = RANDDOUBLE(WIDTH);
-            dna[i].points[j].y = RANDDOUBLE(HEIGHT);
-        }
-        dna[i].r = RANDDOUBLE(1);
-        dna[i].g = RANDDOUBLE(1);
-        dna[i].b = RANDDOUBLE(1);
-        dna[i].a = RANDDOUBLE(1);
-    }
+	for (int i = 0; i < NUM_SHAPES; i++) {
+		for (int j = 0; j < NUM_POINTS; j++) {
+			dna[i].points[j].x = RANDDOUBLE(WIDTH);
+			dna[i].points[j].y = RANDDOUBLE(HEIGHT);
+		}
+		dna[i].r = RANDDOUBLE(1);
+		dna[i].g = RANDDOUBLE(1);
+		dna[i].b = RANDDOUBLE(1);
+		dna[i].a = RANDDOUBLE(1);
+	}
 }
 
 int mutate(void)
 {
-    mutated_shape = RANDINT(NUM_SHAPES);
-    double roulette = RANDDOUBLE(2.8);
-    double drastic = RANDDOUBLE(2);
+	mutated_shape = RANDINT(NUM_SHAPES);
+	double roulette = RANDDOUBLE(2.8);
+	double drastic = RANDDOUBLE(2);
 
-    // mutate color
-    if(roulette<1)
-    {
-        if(dna_test[mutated_shape].a < 0.01 // completely transparent shapes are stupid
-                || roulette<0.25)
-        {
-            if(drastic < 1)
-            {
-                dna_test[mutated_shape].a += RANDDOUBLE(0.1);
-                dna_test[mutated_shape].a = CLAMP(dna_test[mutated_shape].a, 0.0, 1.0);
-            }
-            else
-                dna_test[mutated_shape].a = RANDDOUBLE(1.0);
-        }
-        else if(roulette<0.50)
-        {
-            if(drastic < 1)
-            {
-                dna_test[mutated_shape].r += RANDDOUBLE(0.1);
-                dna_test[mutated_shape].r = CLAMP(dna_test[mutated_shape].r, 0.0, 1.0);
-            }
-            else
-                dna_test[mutated_shape].r = RANDDOUBLE(1.0);
-        }
-        else if(roulette<0.75)
-        {
-            if(drastic < 1)
-            {
-                dna_test[mutated_shape].g += RANDDOUBLE(0.1);
-                dna_test[mutated_shape].g = CLAMP(dna_test[mutated_shape].g, 0.0, 1.0);
-            }
-            else
-                dna_test[mutated_shape].g = RANDDOUBLE(1.0);
-        }
-        else
-        {
-            if(drastic < 1)
-            {
-                dna_test[mutated_shape].b += RANDDOUBLE(0.1);
-                dna_test[mutated_shape].b = CLAMP(dna_test[mutated_shape].b, 0.0, 1.0);
-            }
-            else
-                dna_test[mutated_shape].b = RANDDOUBLE(1.0);
-        }
-    }
-
-    // mutate shape
-    else if(roulette < 2.0)
-    {
-        int point_i = RANDINT(NUM_POINTS);
-        if(roulette<1.5)
-        {
-            if(drastic < 1)
-            {
-                dna_test[mutated_shape].points[point_i].x += (int)RANDDOUBLE(WIDTH/10.0);
-                dna_test[mutated_shape].points[point_i].x = CLAMP(dna_test[mutated_shape].points[point_i].x, 0, WIDTH-1);
-            }
-            else
-                dna_test[mutated_shape].points[point_i].x = RANDDOUBLE(WIDTH);
-        }
-        else
-        {
-            if(drastic < 1)
-            {
-                dna_test[mutated_shape].points[point_i].y += (int)RANDDOUBLE(HEIGHT/10.0);
-                dna_test[mutated_shape].points[point_i].y = CLAMP(dna_test[mutated_shape].points[point_i].y, 0, HEIGHT-1);
-            }
-            else
-                dna_test[mutated_shape].points[point_i].y = RANDDOUBLE(HEIGHT);
-        }
-    }
-
-    // mutate stacking
-    else
-    {
-        int destination = RANDINT(NUM_SHAPES);
-        shape_t s = dna_test[mutated_shape];
-        dna_test[mutated_shape] = dna_test[destination];
-        dna_test[destination] = s;
-        return destination;
-    }
-    return -1;
+	// mutate color
+	if (roulette < 1) {
+		if (dna_test[mutated_shape].a < 0.01	// completely transparent shapes are stupid
+				|| roulette < 0.25) {
+			if (drastic < 1) {
+				dna_test[mutated_shape].a += RANDDOUBLE(0.1);
+				dna_test[mutated_shape].a =
+					CLAMP(dna_test[mutated_shape].a, 0.0, 1.0);
+			} else
+				dna_test[mutated_shape].a = RANDDOUBLE(1.0);
+		} else if (roulette < 0.50) {
+			if (drastic < 1) {
+				dna_test[mutated_shape].r += RANDDOUBLE(0.1);
+				dna_test[mutated_shape].r =
+					CLAMP(dna_test[mutated_shape].r, 0.0, 1.0);
+			} else
+				dna_test[mutated_shape].r = RANDDOUBLE(1.0);
+		} else if (roulette < 0.75) {
+			if (drastic < 1) {
+				dna_test[mutated_shape].g += RANDDOUBLE(0.1);
+				dna_test[mutated_shape].g =
+					CLAMP(dna_test[mutated_shape].g, 0.0, 1.0);
+			} else
+				dna_test[mutated_shape].g = RANDDOUBLE(1.0);
+		} else {
+			if (drastic < 1) {
+				dna_test[mutated_shape].b += RANDDOUBLE(0.1);
+				dna_test[mutated_shape].b =
+					CLAMP(dna_test[mutated_shape].b, 0.0, 1.0);
+			} else
+				dna_test[mutated_shape].b = RANDDOUBLE(1.0);
+		}
+	}
+	// mutate shape
+	else if (roulette < 2.0) {
+		int point_i = RANDINT(NUM_POINTS);
+		if (roulette < 1.5) {
+			if (drastic < 1) {
+				dna_test[mutated_shape].points[point_i].x +=
+					(int) RANDDOUBLE(WIDTH / 10.0);
+				dna_test[mutated_shape].points[point_i].x =
+					CLAMP(dna_test[mutated_shape].points[point_i].x, 0,
+							WIDTH - 1);
+			} else
+				dna_test[mutated_shape].points[point_i].x =
+					RANDDOUBLE(WIDTH);
+		} else {
+			if (drastic < 1) {
+				dna_test[mutated_shape].points[point_i].y +=
+					(int) RANDDOUBLE(HEIGHT / 10.0);
+				dna_test[mutated_shape].points[point_i].y =
+					CLAMP(dna_test[mutated_shape].points[point_i].y, 0,
+							HEIGHT - 1);
+			} else
+				dna_test[mutated_shape].points[point_i].y =
+					RANDDOUBLE(HEIGHT);
+		}
+	}
+	// mutate stacking
+	else {
+		int destination = RANDINT(NUM_SHAPES);
+		shape_t s = dna_test[mutated_shape];
+		dna_test[mutated_shape] = dna_test[destination];
+		dna_test[destination] = s;
+		return destination;
+	}
+	return -1;
 
 }
 
 int MAX_FITNESS = -1;
 
-unsigned char * goal_data = NULL;
+unsigned char *goal_data = NULL;
 
 int difference(cairo_surface_t * test_surf, cairo_surface_t * goal_surf)
 {
-    unsigned char * test_data = cairo_image_surface_get_data(test_surf);
-    if(!goal_data)
-        goal_data = cairo_image_surface_get_data(goal_surf);
+	unsigned char *test_data = cairo_image_surface_get_data(test_surf);
+	if (!goal_data)
+		goal_data = cairo_image_surface_get_data(goal_surf);
 
-    int difference = 0;
+	int difference = 0;
 
-    int my_max_fitness = 0;
+	int my_max_fitness = 0;
 
-    for(int y = 0; y < HEIGHT; y++)
-    {
-        for(int x = 0; x < WIDTH; x++)
-        {
-            int thispixel = y*WIDTH*4 + x*4;
+	for (int y = 0; y < HEIGHT; y++) {
+		for (int x = 0; x < WIDTH; x++) {
+			int thispixel = y * WIDTH * 4 + x * 4;
 
-            unsigned char test_a = test_data[thispixel];
-            unsigned char test_r = test_data[thispixel + 1];
-            unsigned char test_g = test_data[thispixel + 2];
-            unsigned char test_b = test_data[thispixel + 3];
+			unsigned char test_a = test_data[thispixel];
+			unsigned char test_r = test_data[thispixel + 1];
+			unsigned char test_g = test_data[thispixel + 2];
+			unsigned char test_b = test_data[thispixel + 3];
 
-            unsigned char goal_a = goal_data[thispixel];
-            unsigned char goal_r = goal_data[thispixel + 1];
-            unsigned char goal_g = goal_data[thispixel + 2];
-            unsigned char goal_b = goal_data[thispixel + 3];
+			unsigned char goal_a = goal_data[thispixel];
+			unsigned char goal_r = goal_data[thispixel + 1];
+			unsigned char goal_g = goal_data[thispixel + 2];
+			unsigned char goal_b = goal_data[thispixel + 3];
 
-            if(MAX_FITNESS == -1)
-                my_max_fitness += goal_a + goal_r + goal_g + goal_b;
+			if (MAX_FITNESS == -1)
+				my_max_fitness += goal_a + goal_r + goal_g + goal_b;
 
-            difference += ABS(test_a - goal_a);
-            difference += ABS(test_r - goal_r);
-            difference += ABS(test_g - goal_g);
-            difference += ABS(test_b - goal_b);
-        }
-    }
+			difference += ABS(test_a - goal_a);
+			difference += ABS(test_r - goal_r);
+			difference += ABS(test_g - goal_g);
+			difference += ABS(test_b - goal_b);
+		}
+	}
 
-    if(MAX_FITNESS == -1)
-        MAX_FITNESS = my_max_fitness;
-    return difference;
+	if (MAX_FITNESS == -1)
+		MAX_FITNESS = my_max_fitness;
+	return difference;
 }
 
 void copy_surf_to(cairo_surface_t * surf, cairo_t * cr)
 {
-    cairo_set_source_rgb(cr, 1, 1, 1);
-    cairo_rectangle(cr, 0, 0, WIDTH, HEIGHT);
-    cairo_fill(cr);
-    //cairo_set_operator(cr, CAIRO_OPERATOR_SOURCE);
-    cairo_set_source_surface(cr, surf, 0, 0);
-    cairo_paint(cr);
+	cairo_set_source_rgb(cr, 1, 1, 1);
+	cairo_rectangle(cr, 0, 0, WIDTH, HEIGHT);
+	cairo_fill(cr);
+	//cairo_set_operator(cr, CAIRO_OPERATOR_SOURCE);
+	cairo_set_source_surface(cr, surf, 0, 0);
+	cairo_paint(cr);
 }
 
 static void mainloop(cairo_surface_t * pngsurf)
 {
-    struct timeval start;
-    gettimeofday(&start, NULL);
 
-    init_dna(dna_best);
-    memcpy((void *)dna_test, (const void *)dna_best, sizeof(shape_t) * NUM_SHAPES);
+	struct timeval start;
+	gettimeofday(&start, NULL);
 
-#ifdef SHOWWINDOW
-	cairo_surface_t * xsurf = cairo_image_surface_create_for_data (
-		screen->pixels,
-		CAIRO_FORMAT_RGB24,
-		WIDTH, HEIGHT,
-		screen->pitch);
+	init_dna(dna_best);
+	memcpy((void *) dna_test, (const void *) dna_best,
+			sizeof(shape_t) * NUM_SHAPES);
 
-	cairo_t * xcr = cairo_create(xsurf);
+#ifdef WITH_SDL
+	cairo_surface_t *xsurf = NULL;
+	cairo_t *xcr = NULL;
+	if (SHOW_WINDOW) {
+		xsurf = cairo_image_surface_create_for_data(screen->pixels,
+				CAIRO_FORMAT_RGB24,
+				WIDTH, HEIGHT,
+				screen->pitch);
+
+		xcr = cairo_create(xsurf);
+	}
 #endif
 
-    cairo_surface_t * test_surf = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, WIDTH, HEIGHT);
-    cairo_t * test_cr = cairo_create(test_surf);
+	cairo_surface_t *test_surf =
+		cairo_image_surface_create(CAIRO_FORMAT_ARGB32, WIDTH, HEIGHT);
+	cairo_t *test_cr = cairo_create(test_surf);
 
-    cairo_surface_t * goalsurf = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, WIDTH, HEIGHT);
-    cairo_t * goalcr = cairo_create(goalsurf);
-    copy_surf_to(pngsurf, goalcr);
+	cairo_surface_t *goalsurf =
+		cairo_image_surface_create(CAIRO_FORMAT_ARGB32, WIDTH, HEIGHT);
+	cairo_t *goalcr = cairo_create(goalsurf);
+	copy_surf_to(pngsurf, goalcr);
 
-    int lowestdiff = INT_MAX;
-    int teststep = 0;
-    int beststep = 0;
-    for(;;) {
-        int other_mutated = mutate();
-        draw_dna(dna_test, test_cr);
+	int lowestdiff = INT_MAX;
+	int teststep = 0;
+	int beststep = 0;
+	for (;;) {
+		int other_mutated = mutate();
+		draw_dna(dna_test, test_cr);
 
-        int diff = difference(test_surf, goalsurf);
-        if(diff < lowestdiff)
-        {
-            beststep++;
-            // test is good, copy to best
-            dna_best[mutated_shape] = dna_test[mutated_shape];
-            if(other_mutated >= 0)
-                dna_best[other_mutated] = dna_test[other_mutated];
-#ifdef SHOWWINDOW
-            copy_surf_to(test_surf, xcr); // also copy to display
-			SDL_UpdateWindowSurface(win);
+		int diff = difference(test_surf, goalsurf);
+		if (diff < lowestdiff) {
+			beststep++;
+			// test is good, copy to best
+			dna_best[mutated_shape] = dna_test[mutated_shape];
+			if (other_mutated >= 0)
+				dna_best[other_mutated] = dna_test[other_mutated];
+#ifdef WITH_SDL
+			if (SHOW_WINDOW) {
+				copy_surf_to(test_surf, xcr);	// also copy to display
+				SDL_UpdateWindowSurface(win);
+			}
 #endif
-            lowestdiff = diff;
-        }
-        else
-        {
-            // test sucks, copy best back over test
-            dna_test[mutated_shape] = dna_best[mutated_shape];
-            if(other_mutated >= 0)
-                dna_test[other_mutated] = dna_best[other_mutated];
-        }
+			lowestdiff = diff;
+		} else {
+			// test sucks, copy best back over test
+			dna_test[mutated_shape] = dna_best[mutated_shape];
+			if (other_mutated >= 0)
+				dna_test[other_mutated] = dna_best[other_mutated];
+		}
 
-        teststep++;
+		teststep++;
 
 #ifdef TIMELIMIT
-        struct timeval t;
-        gettimeofday(&t, NULL);
-        if(t.tv_sec - start.tv_sec > TIMELIMIT)
-        {
-            printf("%0.6f\n", ((MAX_FITNESS-lowestdiff) / (float)MAX_FITNESS)*100);
+		struct timeval t;
+		gettimeofday(&t, NULL);
+		if (t.tv_sec - start.tv_sec > TIMELIMIT) {
+			printf("%0.6f\n",
+					((MAX_FITNESS -
+					  lowestdiff) / (float) MAX_FITNESS) * 100);
 #ifdef DUMP
-            char filename[50];
-            sprintf(filename, "%d.data", getpid());
-            FILE * f = fopen(filename, "w");
-            fwrite(dna_best, sizeof(shape_t), NUM_SHAPES, f);
-            fclose(f);
+			char filename[50];
+			sprintf(filename, "%d.data", getpid());
+			FILE *f = fopen(filename, "w");
+			fwrite(dna_best, sizeof(shape_t), NUM_SHAPES, f);
+			fclose(f);
 #endif
-            return;
-        }
+			return;
+		}
 #else
-        if(teststep % 100 == 0)
-            printf("Step = %d/%d\nFitness = %0.6f%%\n",
-                    beststep, teststep, ((MAX_FITNESS-lowestdiff) / (float)MAX_FITNESS)*100);
+		if (teststep % 100 == 0)
+			printf("Step = %d/%d\nFitness = %0.6f%%\n",
+					beststep, teststep,
+					((MAX_FITNESS -
+					  lowestdiff) / (float) MAX_FITNESS) * 100);
 #endif
 
-#ifdef SHOWWINDOW
-		/*
-        if(teststep % 100 == 0 && XPending(dpy))
-        {
-            XEvent xev;
-            XNextEvent(dpy, &xev);
-            switch(xev.type) {
-                case Expose:
-                    XCopyArea(dpy, pixmap, win, gc,
-                            xev.xexpose.x, xev.xexpose.y,
-                            xev.xexpose.width, xev.xexpose.height,
-                            xev.xexpose.x, xev.xexpose.y);
-            }
-        }
-		*/
-		SDL_Event event;
-      while (SDL_PollEvent(&event)) {
-		  // printf("Event type: %d\n", event.type);
-		  if (event.type == SDL_QUIT || event.type == SDL_WINDOWEVENT_CLOSE) {
-			  exit(0);
-		  }
-	  }
+#ifdef WITH_SDL
+		if (SHOW_WINDOW) {
+			SDL_Event event;
+			while (SDL_PollEvent(&event)) {
+				// printf("Event type: %d\n", event.type);
+				if (event.type == SDL_QUIT
+						|| event.type == SDL_WINDOWEVENT_CLOSE) {
+					exit(0);
+				}
+			}
+		}
 #endif
-    }
+	}
 }
 
-int main(int argc, char ** argv) {
-    cairo_surface_t * pngsurf;
-    if(argc == 1)
-        pngsurf = cairo_image_surface_create_from_png("mona.png");
-    else
-        pngsurf = cairo_image_surface_create_from_png(argv[1]);
-
-    WIDTH = cairo_image_surface_get_width(pngsurf);
-    HEIGHT = cairo_image_surface_get_height(pngsurf);
-
-    srandom(getpid() + time(NULL));
-    x_init();
-    mainloop(pngsurf);
+void print_help(const char *program)
+{
+	printf(
+"Usage: %s [OPTION]... [FILE]\n"
+"``Evolves'' an image out of triangles from a source image.\n"
+"  input images must be in the PNG format.\n"
+"\n"
+" -s        hides the intermediate view: images will continue to be\n"
+"             generated in the background.\n"
+//" -t TIME   Sets a time limit in seconds. Program will terminate afterward.\n"
+//" -d FILE   Dumps the resulting image to a PNG file named FILE.\n"
+" -h        Displays this help and exits.\n"
+	, program);
 }
 
+int main(int argc, char **argv)
+{
+	cairo_surface_t *pngsurf;
+	char *input_name = "mona.png";
+	char c;
+
+	while ((c = getopt(argc, argv, "svtdh")) != -1) {
+		switch (c) {
+			case 'd':
+				dump = true;
+				break;
+			case 't':
+				timelimit = true;
+				break;
+			case 's':
+				SHOW_WINDOW = false;
+				break;
+			case 'h':
+				print_help(argv[0]);
+				return 0;
+			default:
+				abort();
+		}
+	}
+
+	if (argc - optind == 1) {
+		input_name = argv[optind];
+	}
+
+	pngsurf = cairo_image_surface_create_from_png(input_name);
+
+	WIDTH = cairo_image_surface_get_width(pngsurf);
+	HEIGHT = cairo_image_surface_get_height(pngsurf);
+
+	srandom(getpid() + time(NULL));
+	x_init();
+	mainloop(pngsurf);
+}
