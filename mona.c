ca851935481dbaca517d36279d89eb9ec4fa37da
JanBrennen <jan@janpaal.com>
2014-05-09 20:11:27 -0700

    Fixed some of my own mistakes, edited caps vars

--- mona.c
+++ mona.c
@@ -24,10 +24,11 @@
 #define RANDINT(max)          (int)((random() / (double)RAND_MAX) * (max))
 #define RANDDOUBLE(max)       ((random() / (double)RAND_MAX) * max)
 #define ABS(val)              ((val) < 0 ? -(val) : (val))
-#define CLAMP(val, min, max)  ((val) < (min) ? (min) : (val) > (max) ? (max) : (val))
+#define CLAMP(val, min, max)  ((val) < (min) ? (min) : \
+                               (val) > (max) ? (max) : (val))
 
-int WIDTH;
-int HEIGHT;
+int height, width;
+int mshape;
 
 //////////////////////// X11 stuff ////////////////////////
 #ifdef SHOWWINDOW
@@ -51,12 +52,13 @@ void x_init(void)
 		exit(1);
 	}
 	screen = DefaultScreen(dpy);
-	win = XCreateWindow(dpy, DefaultRootWindow(dpy), 0, 0, WIDTH, HEIGHT,
-	                    0, DefaultDepth(dpy, screen), CopyFromParent,
-	                    DefaultVisual(dpy, screen), CWBackPixmap, &attr);
-	pixmap = XCreatePixmap(dpy, win, WIDTH, HEIGHT,
+	win    = XCreateWindow(dpy, DefaultRootWindow(dpy), 0, 0, width,
+	                       height, 0, DefaultDepth(dpy, screen),
+	                       CopyFromParent, DefaultVisual(dpy, screen),
+	                       CWBackPixmap, &attr);
+	pixmap = XCreatePixmap(dpy, win, width, height,
 	                       DefaultDepth(dpy, screen));
-	gc = XCreateGC(dpy, pixmap, 0, NULL);
+	gc     = XCreateGC(dpy, pixmap, 0, NULL);
 	XSelectInput(dpy, win, ExposureMask);
 	XMapWindow(dpy, win);
 }
@@ -65,47 +67,47 @@ void x_init(void)
 
 typedef struct {
 	double x, y;
-} p_t;
+} p_t; /* point_t */
 
 typedef struct {
 	double r, g, b, a;
 	p_t    ps[NUM_POINTS];
-} shape_t;
+} s_t; /* shape_t */
 
-shape_t dna_best[NUM_SHAPES];
-shape_t dna_test[NUM_SHAPES];
+s_t dna_best[NUM_SHAPES];
+s_t dna_test[NUM_SHAPES];
 
-int mshape;
-
-void draw_shape(shape_t * dna, cairo_t * cr, int i)
+void draw_shape(s_t *dna, cairo_t *cr, int i)
 {
 	int j;
+	s_t *shape = &dna[i];
+
 	cairo_set_line_width(cr, 0);
-	shape_t * shape = &dna[i];
 	cairo_set_source_rgba(cr, shape->r, shape->g, shape->b, shape->a);
 	cairo_move_to(cr, shape->ps[0].x, shape->ps[0].y);
-	for(j = 1; j < NUM_POINTS; j++)
+	for(j = 1; j < NUM_POINTS; j++) {
 		cairo_line_to(cr, shape->ps[j].x, shape->ps[j].y);
+	}
 	cairo_fill(cr);
 }
 
-void draw_dna(shape_t * dna, cairo_t * cr)
+void draw_dna(s_t * dna, cairo_t * cr)
 {
 	int i;
 	cairo_set_source_rgb(cr, 1, 1, 1);
-	cairo_rectangle(cr, 0, 0, WIDTH, HEIGHT);
+	cairo_rectangle(cr, 0, 0, width, height);
 	cairo_fill(cr);
 	for(i = 0; i < NUM_SHAPES; i++)
 		draw_shape(dna, cr, i);
 }
 
-void init_dna(shape_t * dna)
+void init_dna(s_t * dna)
 {
 	int i, j;
 	for(i = 0; i < NUM_SHAPES; i++) {
 		for(j = 0; j < NUM_POINTS; j++) {
-		    dna[i].ps[j].x = RANDDOUBLE(WIDTH);
-		    dna[i].ps[j].y = RANDDOUBLE(HEIGHT);
+		    dna[i].ps[j].x = RANDDOUBLE(width);
+		    dna[i].ps[j].y = RANDDOUBLE(height);
 		}
 		dna[i].r = RANDDOUBLE(1);
 		dna[i].g = RANDDOUBLE(1);
@@ -120,12 +122,12 @@ void init_dna(shape_t * dna)
 
 int mutate(void)
 {
-	mshape = RANDINT(NUM_SHAPES);
+	mshape          = RANDINT(NUM_SHAPES);
 	double roulette = RANDDOUBLE(2.8);
-	double drastic = RANDDOUBLE(2);
+	double drastic  = RANDDOUBLE(2);
 
 	// mutate color
-	if(roulette<1) {
+	if(roulette < 1) {
 		// completely transparent shapes are stupid
 		if(dna_test[mshape].a < 0.01 || roulette<0.25) {
 			if(drastic < 1) {
@@ -136,7 +138,7 @@ int mutate(void)
 			} else {
 				dna_test[mshape].a = RANDDOUBLE(1.0);
 			}
-		} else if(roulette<0.50) {
+		} else if(roulette < 0.50) {
 			if(drastic < 1) {
 				dna_test[mshape].r += RANDDOUBLE(0.1);
 				dna_test[mshape].r = 
@@ -144,7 +146,7 @@ int mutate(void)
 			} else {
 				dna_test[mshape].r = RANDDOUBLE(1.0);
 			}
-		} else if(roulette<0.75) {
+		} else if(roulette < 0.75) {
 			if(drastic < 1) {
 				dna_test[mshape].g += RANDDOUBLE(0.1);
 				dna_test[mshape].g =
@@ -169,26 +171,28 @@ int mutate(void)
 		if(roulette < 1.5) {
 			if(drastic < 1) {
 				dna_test[mshape].ps[p_i].x +=
-				 (int)RANDDOUBLE(WIDTH/10.0);
+				 (int)RANDDOUBLE(width/10.0);
 				dna_test[mshape].ps[p_i].x =
-				 CLAMP(dna_test[mshape].ps[p_i].x, 0, WIDTH-1);
+				 CLAMP(dna_test[mshape].ps[p_i].x, 0, width-1);
 			} else {
-				dna_test[mshape].ps[p_i].x = RANDDOUBLE(WIDTH);
+				dna_test[mshape].ps[p_i].x = RANDDOUBLE(width);
 			}
 		} else {
 			if(drastic < 1) {
 				dna_test[mshape].ps[p_i].y +=
-				 (int)RANDDOUBLE(HEIGHT/10.0);
+				 (int)RANDDOUBLE(height/10.0);
 				dna_test[mshape].ps[p_i].y =
-				 CLAMP(dna_test[mshape].ps[p_i].y, 0, HEIGHT-1);
+				 CLAMP(dna_test[mshape].ps[p_i].y, 0,
+				       height-1);
 			} else {
-				dna_test[mshape].ps[p_i].y = RANDDOUBLE(HEIGHT);
+				dna_test[mshape].ps[p_i].y =
+				 RANDDOUBLE(height);
 			}
 		}
     // mutate stacking
 	} else {
 		int destination = RANDINT(NUM_SHAPES);
-		shape_t s = dna_test[mshape];
+		s_t s = dna_test[mshape];
 		dna_test[mshape] = dna_test[destination];
 		dna_test[destination] = s;
 		return destination;
@@ -212,9 +216,9 @@ int difference(cairo_surface_t * test_surf, cairo_surface_t * goal_surf)
 	int difference = 0;
 	int my_max_fitness = 0;
 
-	for(y = 0; y < HEIGHT; y++) {
-		for(x = 0; x < WIDTH; x++) {
-			int thispixel = y*WIDTH*4 + x*4;
+	for(y = 0; y < height; y++) {
+		for(x = 0; x < width; x++) {
+			int thispixel = y*width*4 + x*4;
 
 			unsigned char test_a = test_data[thispixel];
 			unsigned char test_r = test_data[thispixel + 1];
@@ -246,7 +250,7 @@ int difference(cairo_surface_t * test_surf, cairo_surface_t * goal_surf)
 void copy_surf_to(cairo_surface_t * surf, cairo_t * cr)
 {
 	cairo_set_source_rgb(cr, 1, 1, 1);
-	cairo_rectangle(cr, 0, 0, WIDTH, HEIGHT);
+	cairo_rectangle(cr, 0, 0, width, height);
 	cairo_fill(cr);
 	//cairo_set_operator(cr, CAIRO_OPERATOR_SOURCE);
 	cairo_set_source_surface(cr, surf, 0, 0);
@@ -260,21 +264,21 @@ static void mainloop(cairo_surface_t * pngsurf)
 
 	init_dna(dna_best);
 	memcpy((void *)dna_test, (const void *)dna_best,
-	       sizeof(shape_t) * NUM_SHAPES);
+	       sizeof(s_t) * NUM_SHAPES);
 
 #ifdef SHOWWINDOW
 	cairo_surface_t * xsurf =
 	 cairo_xlib_surface_create(dpy, pixmap, DefaultVisual(dpy, screen),
-	                           WIDTH, HEIGHT);
+	                           width, height);
 	cairo_t * xcr = cairo_create(xsurf);
 #endif
 
 	cairo_surface_t * test_surf =
-	 cairo_image_surface_create(CAIRO_FORMAT_ARGB32, WIDTH, HEIGHT);
+	 cairo_image_surface_create(CAIRO_FORMAT_ARGB32, width, height);
 	cairo_t * test_cr = cairo_create(test_surf);
 
 	cairo_surface_t * goalsurf =
-	 cairo_image_surface_create(CAIRO_FORMAT_ARGB32, WIDTH, HEIGHT);
+	 cairo_image_surface_create(CAIRO_FORMAT_ARGB32, width, height);
 	cairo_t * goalcr = cairo_create(goalsurf);
 	copy_surf_to(pngsurf, goalcr);
 
@@ -296,7 +300,7 @@ static void mainloop(cairo_surface_t * pngsurf)
 			}
 #ifdef SHOWWINDOW
 			copy_surf_to(test_surf, xcr); // also copy to display
-			XCopyArea(dpy, pixmap, win, gc, 0, 0, WIDTH, HEIGHT,
+			XCopyArea(dpy, pixmap, win, gc, 0, 0, width, height,
 			 0, 0);
 #endif
 			lowestdiff = diff;
@@ -316,13 +320,14 @@ static void mainloop(cairo_surface_t * pngsurf)
 		gettimeofday(&t, NULL);
 		if(t.tv_sec - start.tv_sec > TIMELIMIT) {
 			printf("%0.6f\n",
-			       ((MAX_FITNESS-lowestdiff) / (float)MAX_FITNESS)*100);
+			       ((MAX_FITNESS-lowestdiff) /
+				 (float)MAX_FITNESS)*100);
 		}
 #ifdef DUMP
 		char filename[50];
 		sprintf(filename, "%d.data", getpid());
 		FILE * f = fopen(filename, "w");
-		fwrite(dna_best, sizeof(shape_t), NUM_SHAPES, f);
+		fwrite(dna_best, sizeof(s_t), NUM_SHAPES, f);
 		fclose(f);
 #endif
 		return;
@@ -339,9 +344,9 @@ static void mainloop(cairo_surface_t * pngsurf)
 		XEvent xev;
 		XNextEvent(dpy, &xev);
 		if(xev.type == Expose) {
-			XCopyArea(dpy, pixmap, win, gc,
-			          xev.xexpose.x, xev.xexpose.y, xev.xexpose.width,
-			          xev.xexpose.height, xev.xexpose.x, xev.xexpose.y);
+			XCopyArea(dpy, pixmap, win, gc, xev.xexpose.x,
+			 xev.xexpose.y, xev.xexpose.width, xev.xexpose.height,
+			 xev.xexpose.x, xev.xexpose.y);
 		}
 	}
 #endif
@@ -356,12 +361,11 @@ int main(int argc, char ** argv) {
 		pngsurf = cairo_image_surface_create_from_png(argv[1]);
 	}
 
-	WIDTH = cairo_image_surface_get_width(pngsurf);
-	HEIGHT = cairo_image_surface_get_height(pngsurf);
+	width = cairo_image_surface_get_width(pngsurf);
+	height = cairo_image_surface_get_height(pngsurf);
 
 	srandom(getpid() + time(NULL));
 	x_init();
 	mainloop(pngsurf);
 	return 0;
 }
-
